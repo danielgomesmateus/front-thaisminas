33d02293aeb5d97aa5f01d549967cb2198ac98aa
Daniel Gomes <daniel.gomes@provafacilnaweb.com.br>
2020-05-16 15:54:46 -0300

    Ajuste na funcionalidade de download de arquivos do projeto

--- src/views/Projects/ViewProject.vue
+++ src/views/Projects/ViewProject.vue
@@ -2,6 +2,9 @@
   <v-app>
     <v-container>
       <v-row>
+        <v-col cols="12" md="12" v-if="alert.value">    
+          <alert :alert="alert" />
+        </v-col>
         <v-col cols="12" md="12">
           <h1 class="display-1 text-center">
             {{ project.name }}
@@ -15,21 +18,18 @@
           <div class="text-left" v-html="project.content">
           </div>
         </v-col>
-        <!--
         <v-col cols="12" md="12">
-
           <v-btn
+            small
+            dark
             color="success"
-            class="ma-2 white--text"
+            class="white--text"
             @click="downloadProject"
           >
-            Baixar projeto
-            <v-icon right dark>mdi-cloud-download</v-icon>
+            <v-icon>mdi-cloud-download</v-icon> <span class="font-weight-bold"> Baixar projeto</span>
           </v-btn>
           <v-dialog
             v-model="dialog"
-            hide-overlay
-            persistent
             width="300"
           >
             <v-card
@@ -37,7 +37,7 @@
               dark
             >
               <v-card-text>
-                Aguarde um momento.
+                Aguarde um momento...
                 <v-progress-linear
                   indeterminate
                   color="white"
@@ -47,19 +47,29 @@
             </v-card>
           </v-dialog>
         </v-col>
-        -->
       </v-row>
     </v-container>
   </v-app>
 </template>
 <script>
+  import Alert from '../../components/Alerts/Alert'
+
   import { mapGetters } from 'vuex'
+  import axios from 'axios'
 
   export default {
     data() {
       return {
-        dialog: false
+        dialog: false,
+        alert: {
+          value: false,
+          type: '',
+          message: ''
+        }
       }
+    },
+    components: {
+      'alert': Alert
     },
     computed: {
       ...mapGetters({
@@ -73,6 +83,30 @@
     methods: {
       downloadProject() {
         this.dialog =  true
+        
+        axios({
+          method: 'GET',
+          url: `${ process.env.VUE_APP_BASE_URL_API }files/${ this.project.slug }`,
+          responseType: 'blob'
+        })
+        .then(response => {
+          const fileURL = window.URL.createObjectURL(new Blob([response.data]))
+          let fileLink = document.createElement('a')  
+
+          fileLink.href = fileURL
+          fileLink.setAttribute('download', `${ this.project.name }.zip`)
+          document.body.appendChild(fileLink)   
+          fileLink.click()
+
+          this.dialog = false
+        })
+        .catch(error => {
+          this.dialog = false
+
+          this.alert.value = true
+          this.alert.type = 'error'
+          this.alert.message = error.message
+        })
       }
     }
   }
